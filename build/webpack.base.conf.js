fd1a364e249d288037c177398752993607bd92a6
uapasha <uapasha@gmail.com>
2017-01-17 20:18:07 +0200

    add jquery

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
+var webpack = require('webpack');
 
 var env = process.env.NODE_ENV
 // check env & config/index.js to decide whether to enable CSS source maps for the
@@ -102,5 +103,13 @@ module.exports = {
         browsers: ['last 2 versions']
       })
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // jquery
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    })
+  ]
 }
