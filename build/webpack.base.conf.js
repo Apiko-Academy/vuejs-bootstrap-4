57a33e6a6a3a224718743bc2ae8a843fff0af5b3
uapasha <uapasha@gmail.com>
2017-01-17 20:17:15 +0200

    add sass support

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,6 +9,7 @@ var env = process.env.NODE_ENV
 var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
 var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
+var sassPath = path.resolve(__dirname, './src/styles/')
 
 module.exports = {
   entry: {
@@ -26,7 +27,8 @@ module.exports = {
       'vue$': 'vue/dist/vue.common.js',
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'style': sassPath
     }
   },
   resolveLoader: {
@@ -83,6 +85,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!postcss!sass?sourceMap'
       }
     ]
   },
